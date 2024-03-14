c1f6f68390c7ba2446e1be831312a3a78b6298aa
briquetech <jayvpagnis@gmail.com>
2024-03-14 17:19:34 +0530

    1. Updated package.json with missing packages, 2. Updated app.js with entries for components and mixins

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,12 +4,31 @@ import { createApp } from 'vue'
 import LeftNavbarComponent from './components/LeftNavbarComponent';
 import ExampleComponent from "./components/ExampleComponent";
 
+// Mixins
+import SWAlert from "./mixins/swal";
+import SWUtils from "./mixins/utils";
+import Masters from "./mixins/masters";
+
+// Datatables
+import DatatableComponent from './components/common/DatatableComponent.vue';
+// All form errors
+import ErrorComponent from "./components/common/ErrorComponent.vue";
+
 const app = createApp({})
+
+// Components
+app.component("DataTableComponent", DatatableComponent);
+app.component("error", ErrorComponent);
 
 // Nav
 app.component('left-nav-component', LeftNavbarComponent);
 
 // Pages
 app.component("example", ExampleComponent);
+
+// Mixins
+app.mixin(SWAlert);
+app.mixin(SWUtils);
+app.mixin(Masters);
 
 app.mount('#app');
